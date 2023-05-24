185b94eab217da709c384f7878d7a6f41781f3f3
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-05-24 21:39:55 +0300

    Add static asset resolving

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = 5000;
 
-// Handlebars configuration
+// Express configoration
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// Handlebars configoration
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
