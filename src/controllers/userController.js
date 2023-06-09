daa48517e4ef8b29ca3216c6580a8cd7149ddfc1
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-06-09 10:52:55 +0300

    Hash password before save

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,7 @@ router.post('/register', async(req, res) => {
 
     await userManager.register({ username, password, repeatPassword });
 
-    res.redirect('users/login');
+    res.redirect('/users/login');
 })
 
 module.exports = router;
